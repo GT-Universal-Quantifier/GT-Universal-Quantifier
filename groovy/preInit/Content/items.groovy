a98fac5d6cbc53b974c6af971d23ffe1605e81f5
akirapriva <eymencagan88@gmail.com>
2026-01-26 16:39:50 +0300

    added crude rock shovel & disabled being able to break gravel with fist

--- groovy/preInit/Content/items.groovy
+++ groovy/preInit/Content/items.groovy
@@ -1,3 +1,8 @@
+import net.minecraft.item.ItemSpade
+import net.minecraft.item.Item
+import net.minecraft.item.Item.ToolMaterial
+import net.minecraftforge.common.util.EnumHelper
+
 content.createItem('stone_shard')
     .register()
 content.createItem('rawhide')
@@ -6,3 +11,21 @@ content.createItem('soaked_rawhide')
     .register()
 content.createItem('dried_rawhide')
     .register()
+
+def CRUDE_STONE = EnumHelper.addToolMaterial(
+    "CRUDE_ROCK",
+    0,      // harvest level (wood)
+    32,     // durability (very low, realistic)
+    1.5f,   // mining speed (slow)
+    0.0f,   // attack damage
+    2       // enchantability
+)
+
+content.registerItem('rock_fiber_crude_shovel',
+    new ItemSpade(CRUDE_STONE) {
+        {
+            setFull3D()
+            setMaxStackSize(1)
+        }
+    }
+)
